31cef2dea4e4007c33a1bee043e4f5662201f920
Ykiiii <yangkaiiiiii@163.com>
2023-12-05 15:08:33 +0800

    202312051508

--- check_poses_train_imgs.py
+++ check_poses_train_imgs.py
@@ -20,21 +20,22 @@ im_step = 100 # Consider every im_step-th image
 
 # Path to the T-LESS dataset
 # Which you can download using the t-less_download.py script. 
-data_path = '/Add/your/path/here/t-less_v2'
+data_path = 'E:/DataSets/t-less_v2'
+
 
 # Path to the folder in which the images produced by this script will be saved
 output_dir = os.path.join(data_path, 'output_check_poses_train_imgs')
 
 # Paths to the elements of the T-LESS dataset
-model_path_mask = os.path.join(data_path, 'models_' + model_type, 'obj_{:02d}.ply')
-obj_info_path_mask = os.path.join(data_path, 'train_{}', '{:02d}', 'info.yml')
-obj_gt_path_mask = os.path.join(data_path, 'train_{}', '{:02d}', 'gt.yml')
-rgb_path_mask = os.path.join(data_path, 'train_{}', '{:02d}', 'rgb', '{:04d}.{}')
-depth_path_mask = os.path.join(data_path, 'train_{}', '{:02d}', 'depth', '{:04d}.png')
+model_path_mask = os.path.join(data_path, 'models_' + model_type, 'obj_{}.ply')
+obj_info_path_mask = os.path.join(data_path, 'train_{}', '{}', 'info.yml')
+obj_gt_path_mask = os.path.join(data_path, 'train_{}', '{}', 'gt.yml')
+rgb_path_mask = os.path.join(data_path, 'train_{}', '{}', 'rgb', '{}.{}')
+depth_path_mask = os.path.join(data_path, 'train_{}', '{}', 'depth', '{}.png')
 rgb_ext = {'primesense': 'png', 'kinect': 'png', 'canon': 'jpg'}
 obj_colors_path = os.path.join('data', 'obj_rgb.txt')
-vis_rgb_path_mask = os.path.join(output_dir, '{:02d}_{}_{}_{:04d}_rgb.png')
-vis_depth_path_mask = os.path.join(output_dir, '{:02d}_{}_{}_{:04d}_depth_diff.png')
+vis_rgb_path_mask = os.path.join(output_dir, '{}_{}_{}_{}_rgb.png')
+vis_depth_path_mask = os.path.join(output_dir, '{}_{}_{}_{}_depth_diff.png')
 
 misc.ensure_dir(output_dir)
 obj_colors = inout.load_colors(obj_colors_path)
@@ -42,23 +43,25 @@ obj_colors = inout.load_colors(obj_colors_path)
 plt.ioff() # Turn interactive plotting off
 
 for obj_id in obj_ids:
-
+    
     # Load object model
-    model_path = model_path_mask.format(obj_id)
+    model_path = model_path_mask.format('{:02d}'.format(obj_id))
     model = inout.load_ply(model_path)
 
     # Load info about the templates (including camera parameters etc.)
-    obj_info_path = obj_info_path_mask.format(device, obj_id)
+    obj_info_path = obj_info_path_mask.format(device, '{:02d}'.format(obj_id))
     obj_info = inout.load_info(obj_info_path)
 
-    obj_gt_path = obj_gt_path_mask.format(device, obj_id)
+    obj_gt_path = obj_gt_path_mask.format(device, '{:02d}'.format(obj_id))
     obj_gt = inout.load_gt(obj_gt_path)
 
     for im_id in obj_info.keys():
+        
         if im_id % im_step != 0:
             continue
         print('obj: ' + str(obj_id) + ', device: ' + device + ', im_id: ' + str(im_id))
 
+        
         im_info = obj_info[im_id]
         im_gt = obj_gt[im_id]
 
@@ -70,7 +73,7 @@ for obj_id in obj_ids:
         # Visualization #1
         #-----------------------------------------------------------------------
         # Load RGB image
-        rgb_path = rgb_path_mask.format(device, obj_id, im_id, rgb_ext[device])
+        rgb_path = rgb_path_mask.format(device, '{:02d}'.format(obj_id), '{:04}'.format(im_id), rgb_ext[device])
         rgb = imageio.imread(rgb_path)
 
         # Render RGB image of the object model at the pose associated with
@@ -81,22 +84,25 @@ for obj_id in obj_ids:
         ren_rgb = renderer.render(model, im_size, K, R, t,
                                   surf_color=surf_color, mode='rgb')
 
-        vis_rgb = 0.5 * rgb.astype(np.float) + 0.5 * ren_rgb.astype(np.float)
-        vis_rgb = vis_rgb.astype(np.uint8)
+        # vis_rgb = 0.5 * rgb.astype(np.float) + 0.5 * ren_rgb.astype(np.float)
+        # vis_rgb = vis_rgb.astype(np.uint8)
+
+        vis_rgb = 0.5 * rgb + 0.5 * ren_rgb
+        # vis_rgb = vis_rgb.astype(np.uint8)
 
         # Draw the bounding box of the object
         vis_rgb = misc.draw_rect(vis_rgb, im_gt[0]['obj_bb'])
 
         # Save the visualization
         vis_rgb[vis_rgb > 255] = 255
-        vis_rgb_path = vis_rgb_path_mask.format(obj_id, device, model_type, im_id)
+        vis_rgb_path = vis_rgb_path_mask.format('{:02d}'.format(obj_id), device, model_type, '{:04}'.format(im_id))
         imageio.imwrite(vis_rgb_path, vis_rgb.astype(np.uint8))
 
         # Visualization #2
         #-----------------------------------------------------------------------
         if device != 'canon':
             # Load depth image
-            depth_path = depth_path_mask.format(device, obj_id, im_id, rgb_ext[device])
+            depth_path = depth_path_mask.format(device, '{:02d}'.format(obj_id), '{:04}'.format(im_id), rgb_ext[device])
             depth = imageio.imread(depth_path)  # Unit: 0.1 mm
             depth = depth.astype(np.float) * 0.1  # Convert to mm
 
@@ -111,8 +117,8 @@ for obj_id in obj_ids:
             depth_diff = valid_mask * (depth - ren_depth.astype(np.float))
 
             # Save the visualization
-            vis_depth_path = vis_depth_path_mask.format(obj_id, device,
-                                                        model_type, im_id)
+            vis_depth_path = vis_depth_path_mask.format('{:02d}'.format(obj_id), device,
+                                                        model_type, '{:04}'.format(im_id))
             plt.matshow(depth_diff)
             plt.title('captured - rendered depth [mm]')
             plt.colorbar()
